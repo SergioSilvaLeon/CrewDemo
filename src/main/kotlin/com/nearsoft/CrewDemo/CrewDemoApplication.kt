aaf85c941fe969c807ca799a1b12b68f2a558fa2
Sergio Silva <ssilva@nearsoft.com>
2019-07-18 15:03:35 -0700

    Initial commit

---
+++ src/main/kotlin/com/nearsoft/CrewDemo/CrewDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.nearsoft.CrewDemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrewDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<CrewDemoApplication>(*args)
+}
